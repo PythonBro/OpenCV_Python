ba5318d9e58631f9923612b4debc29691a46bf34
aegis-katoh <33014613+aegis-katoh@users.noreply.github.com>
2017-11-01 13:39:10 +0900

    no message

--- PiCamera/Sample.py
+++ PiCamera/Sample.py
@@ -1,7 +1,8 @@
 import time
 import picamera
-with picamera.PiCamera() as camera:
-    camera.start_preview()
-    time.sleep(2)
+
+camera = picamera.PiCamera()
+camera.start_preview()
+time.sleep(2)
 #	camera.capture('/home/pi/test.jpg')
-    camera.stop_preview()
+camera.stop_preview()
